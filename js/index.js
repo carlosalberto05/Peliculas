66122e25c751e0e777fbd0b4f7a5e411712e255d
carlosalberto05 <carlos_alberto-lira@outlook.com>
2019-11-28 16:36:07 -0600

    Se hizo petcion fetch para obtener las peliculas con nuestra API

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
+const URL_PATH = 'https://api.themoviedb.org';
+const API_KEY = '128402215172fea4f96566ae019f6b5d';
 
+// https://api.themoviedb.org/3/movie/now_playing?api_key=128402215172fea4f96566ae019f6b5d&language=es-ES&page=1
+
+//Evento
+document.addEventListener('DOMContentLoaded', async () => {
+	const movies = await getNewsMovies();
+});
+
+//Método que nos devolverá todas las peliculas
+const getNewsMovies = () => {
+	const url = ` ${URL_PATH}/3/movie/now_playing?api_key=${API_KEY}&language=es-ES&page=1`;
+
+	//Petición fetch
+	return fetch(url)
+		.then(response => response.json())
+		.then(result => result.results)
+		.catch(error => console.log(error));
+};
